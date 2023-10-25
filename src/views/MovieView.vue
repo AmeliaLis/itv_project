ca88c2563179945a71a6a26ce062955148679aa2
AmeliaLis <lis.amelia.maria@gmail.com>
2023-10-25 21:51:35 +0200

    Add film to favorite database

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,40 +1,53 @@
 <template>
     <div>
-        <h2>{{movie.Title}}</h2>
-        <p>{{ movie.Year }}</p>
-        <img :src="movie.Poster" alt="Movie Poster" />
-        <p>{{ movie.Plot }}</p>
-        <p>{{ movie.Actors }}</p>
+      <h2>{{ movie.Title }}</h2>
+      <p>{{ movie.Year }}</p>
+      <img :src="movie.Poster" alt="Movie Poster" />
+      <p>{{ movie.Plot }}</p>
+      <p>{{ movie.Actors }}</p>
     </div>
     <div>
-        <button>Click here to add to your favorites movies</button>
+      <button @click="addToFavorites">
+        Click here to add to your favorites movies
+      </button>
     </div>
-</template>
-
-
-<script>
-import env from "@/env"
-import { ref, onBeforeMount } from 'vue';
-import { useRoute } from "vue-router";
-
-export default {
+  </template>
+  
+  <script>
+  import env from "@/env";
+  import { onAuthStateChanged, getAuth } from "firebase/auth";
+  import { addDoc, collection } from "firebase/firestore";
+  import { db } from "@/main";
+  
+  export default {
     name: "MovieView",
-    setup() {
-        const movie = ref({});
-        const route = useRoute();
-
-        onBeforeMount(() => {
-            fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
-                .then(response => response.json())
-                .then(data => {
-                    movie.value = data;
-                    console.log(movie.value)
-                });
+    mounted() {
+      fetch(
+        `http://www.omdbapi.com/?apikey=${env.apikey}&i=${this.$route.params.id}&plot=full`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.movie = data;
+          console.log(this.movie);
         });
-        return {
-            movie
-        }
-    }
-}
-
-</script>
+    },
+    data() {
+      return {
+        movie: "",
+      };
+    },
+    methods: {
+      addToFavorites() {
+        return new Promise((resolve) => {
+          onAuthStateChanged(getAuth(), (user) => {
+            addDoc(collection(db, "UsersFavoriteFilms"), {
+              data: this.movie,
+              userId: user.uid,
+            });
+            resolve();
+          });
+        });
+      },
+    },
+  };
+  </script>
