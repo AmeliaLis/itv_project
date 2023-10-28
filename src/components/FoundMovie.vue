168937ff3ecdfae4e2e7883d73ed31b39c7f4282
AmeliaLis <lis.amelia.maria@gmail.com>
2023-10-28 16:01:42 +0200

    Fav movie page and display

---
+++ src/components/FoundMovie.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card" style="width: 18rem; max-height: 900px">
+        <img v-bind:src=movie.Poster style="max-height: 400px" class="card-img-top" alt="moviePoster">
+        <div class="card-body">
+          <h5 class="card-title">{{movie.Title}}</h5>
+          <div class="overflow-auto" style="max-height: 200px">
+            <p class="card-text">{{movie.Plot}}</p>
+          </div>
+          
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">{{movie.Year}}</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+        </ul>
+        <div class="card-body">
+          <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    name: "FoundMovieComponent",
+    props: {
+        movieData: {
+            typeof: Object,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            movie: this.movieData.data().data
+        }
+    }
+}
+</script>
--- src/views/ProfileFavoriteView.vue
+++ src/views/ProfileFavoriteView.vue
@@ -1,8 +1,11 @@
 <template>
     <div> this is profile page with favorite films</div>
-    {{ userId}}
-    <div :v-for="movie in movieData">
-        <div> {{movie.userId}}</div>
+    <div class="container-fluid d-flex flex-wrap justify-content-center">
+        <div class="container-fluid d-flex flex-wrap justify-content-center">
+            <div v-for="(movie, index) in favMovies" :key="index">
+                <FavMovie :movieData="movie"></FavMovie>
+            </div>
+        </div>
     </div>
 
 </template>
@@ -11,15 +14,19 @@
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "@/main"
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import FavMovie from "@/components/FavMovie.vue";
 
 
 export default {
     name: "ProfileFavoriteView",
     data() {
         return {
-            movieData: [],
-            userId: undefined
+            favMovies: undefined
         }
+    },
+
+    components: {
+        FavMovie
     },
 
     //to fix
@@ -29,11 +36,7 @@ export default {
                 console.log(user.uid)
                 const q = query(collection(db, "UsersFavoriteFilms"), where("userId", "==", user.uid))
                 getDocs(q).then((result) => {
-                    result.forEach((doc) => {
-                        this.movieData.push(doc.data())
-                        this.userId = doc.data().userId
-                        console.log(this.movie)
-                    });
+                    this.favMovies = result.docs
                 })
                 resolve()
             })
